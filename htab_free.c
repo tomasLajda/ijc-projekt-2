13142e60f3e1075938d6e824084ff26933226658
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:17:12 +0200

    changed order of free

--- htab_free.c
+++ htab_free.c
@@ -1,10 +1,15 @@
+// Author: Tomáš Lajda <xlajdat00>
+// Date: 2024-04-25
+// Description: Frees hash table
+
 #include "htab.h"
 #include "htab_private.h"
 
 void htab_free(htab_t * t) {
     htab_clear(t);
+    
+    free(t->arr);
     t->arr_size = 0;
-    t->size = 0;
     free(t);
     t = NULL;
 }
