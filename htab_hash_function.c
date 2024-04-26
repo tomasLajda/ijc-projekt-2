e98873a29cebb0c8e181b9a1ea680177c736c4b9
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:18:02 +0200

    changed param type

--- htab_hash_function.c
+++ htab_hash_function.c
@@ -1,6 +1,6 @@
 #include "htab.h"
 
-size_t htab_hash_function(const char *str) {
+size_t htab_hash_function(htab_key_t str) {
   uint32_t h=0;     // musí mít 32 bitů
   const unsigned char *p;
   for(p = (const unsigned char*)str; *p != '\0'; p++)
