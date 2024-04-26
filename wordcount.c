b0e871a7a1edf58b77ecccc6e4536215d12b3136
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:21:24 +0200

    implemented wordcount

---
+++ wordcount.c
@@ -0,0 +1,54 @@
+// Author: Tomáš Lajda <xlajdat00>
+// Date: 2024-04-25
+// Description: Wordcount program
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// custom header files
+#include "io.h"
+#include "htab.h"
+
+// I used this formula to determine the size of hash table
+// Expected number of elements = 100,000
+// Load factor = 0.7
+// Initial hash table size = Expected number of elements / Load factor
+// Initial hash table size = 100,000 / 0.7
+// Initial hash table size ≈ 142,857
+#define HTAB_SIZE 143000
+#define MAX_WORD_LEN 255
+
+void print_htab_pair(htab_pair_t *pair) {
+    printf("%s\t%d\n", pair->key, pair->value);
+}
+
+int main() {
+    htab_t *t = htab_init(HTAB_SIZE);
+    if(t == NULL) {
+        fprintf(stderr, "Error: Couldn't allocate memory for hash table.\n");
+        return 1;
+    }
+
+    char word[MAX_WORD_LEN];
+    while (read_word(word, MAX_WORD_LEN, stdin) != EOF) {
+        if(word[0] != '\0') {
+            htab_pair_t *pair = htab_lookup_add(t, word);
+            if(pair == NULL) {
+                fprintf(stderr, "Error: Couldn't allocate memory for new item.\n");
+                htab_free(t);
+                return 1;
+            }
+
+            (pair->value)++;
+        }
+    }
+    
+    htab_for_each(t, print_htab_pair);
+
+    #ifdef STATISTICS 
+    htab_statistics(t);
+    #endif
+
+    htab_free(t);
+    return 0;
+}
