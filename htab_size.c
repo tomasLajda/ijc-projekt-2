a1e9b3d9e0f0dd927d8f438ce72140b9dfad83e0
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:19:45 +0200

    added comment

--- htab_size.c
+++ htab_size.c
@@ -1,6 +1,10 @@
+// Author: Tomáš Lajda <xlajdat00>
+// Date: 2024-04-25
+// Description: Returns number of items in hash table
+
 #include "htab.h"
 #include "htab_private.h"
 
-size_t htab_bucket_count(const htab_t * t) {
-    return t->arr_size;
+size_t htab_size(const htab_t * t) {
+    return t->size;
 }
