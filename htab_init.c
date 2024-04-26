4d3452fa796064e03d504c794fc80c277ecb2e45
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:18:28 +0200

    changed malloc to alloc for buckets

--- htab_init.c
+++ htab_init.c
@@ -5,16 +5,19 @@
 #include "htab.h"
 #include "htab_private.h"
 
-htab_t *htab_init(size_t n) {
-  htab_t *t = malloc(sizeof(htab_t) + n * sizeof(htab_item_t *));
+htab_t *htab_init(const size_t n) {
+  htab_t *t = malloc(sizeof(htab_t));
   if (t == NULL) {
     return NULL;
   }
 
   t->arr_size = n;
   t->size = 0;
-  for (size_t i = 0; i < n; i++) {
-    t->arr[i] = NULL;
+  
+  t->arr = calloc(n, sizeof(htab_item_t*));
+  if (t->arr == NULL) {
+    free(t);
+    return NULL;
   }
 
   return t;
