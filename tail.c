a2fcbbfa58ebb5e4108e540b2777e9366b6759a4
Tomas Lajda <tomas.lajda0@gmail.com>
2024-04-26 12:21:08 +0200

    changed type to cycle_buffer_t

--- tail.c
+++ tail.c
@@ -1,3 +1,7 @@
+// Author: Tomáš Lajda <xlajdat00>
+// Date: 2024-04-12
+// Description: Tail program
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,41 +10,42 @@
 #define MAX_LINE_LENGTH 2048
 #define DEFAULT_SIZE_BUFFER 10
 
-typedef struct cb
-{
+typedef struct cycle_buffer {
   size_t start;
   size_t end;
   size_t size;
   char** lines;
-} cb;
+} cycle_buffer_t;
 
-cb* cbuf_create(size_t n) {
-  cb* buffer = malloc(sizeof(cb));
+// Cycle buffer initialization
+cycle_buffer_t* cbuf_create(size_t n) {
+  cycle_buffer_t* cb = malloc(sizeof(cycle_buffer_t));
 
-  if(buffer == NULL) {
+  if(cb == NULL) {
     fprintf(stderr, "Error: Memory allocation failed\n");
     exit(EXIT_FAILURE);
   }
 
-  buffer->start = 0;
-  buffer->end = 0;
-  buffer->size = n;
+  cb->start = 0;
+  cb->end = 0;
+  cb->size = n;
 
-  buffer->lines = malloc(buffer->size * sizeof(char*));
+  cb->lines = malloc(cb->size * sizeof(char*));
 
-  if(buffer->lines == NULL) {
+  if(cb->lines == NULL) {
     fprintf(stderr, "Error: Memory allocation failed\n");
-    free(buffer);
+    free(cb);
     exit(EXIT_FAILURE);
   }
 
-  for (size_t i = 0; i < buffer->size; i++) {
-    buffer->lines[i] = NULL;
+  for (size_t i = 0; i < cb->size; i++) {
+    cb->lines[i] = NULL;
   }
 
-  return buffer;
+  return cb;
 } 
 
+// Free string line
 void line_free(char* line) {
   if(line != NULL) {
     free(line);
@@ -48,56 +53,59 @@ void line_free(char* line) {
   }
 }
 
-void cbuf_free(cb* buffer) {
-  for (size_t i = 0; i < buffer->size; i++) {
-    if(buffer->lines[i] != NULL) {
-      line_free(buffer->lines[i]);
+// Free cycle buffer
+void cbuf_free(cycle_buffer_t* cb) {
+  for (size_t i = 0; i < cb->size; i++) {
+    if(cb->lines[i] != NULL) {
+      line_free(cb->lines[i]);
     }
   }
 
-  free(buffer->lines);
-  buffer->lines = NULL;
-  free(buffer);
-  buffer = NULL;
+  free(cb->lines);
+  cb->lines = NULL;
+  free(cb);
+  cb = NULL;
 }
 
-void cbuf_put(cb* buffer, char* line) {
-  if(buffer->lines[buffer->end] != NULL) {
-    line_free(buffer->lines[buffer->end]);
+// Put line into cycle buffer
+void cbuf_put(cycle_buffer_t* cb, char* line) {
+  if(cb->lines[cb->end] != NULL) {
+    line_free(cb->lines[cb->end]);
   }
 
-  buffer->lines[buffer->end] = malloc(strlen(line) + 1);
-  if(buffer->lines[buffer->end] == NULL) {
-    cbuf_free(buffer);
+  cb->lines[cb->end] = malloc(strlen(line) + 1);
+  if(cb->lines[cb->end] == NULL) {
+    cbuf_free(cb);
     fprintf(stderr, "Error: Memory allocation failed\n");
     exit(EXIT_FAILURE);
   }
 
-  strcpy(buffer->lines[buffer->end], line);
+  strcpy(cb->lines[cb->end], line);
 
-  if(buffer->end + 1 == buffer->size) {
-    buffer->end = 0;
-    buffer->start = 0;
+  if(cb->end + 1 == cb->size) {
+    cb->end = 0;
+    cb->start = 0;
   } else {
-    if(buffer->end == buffer->start && buffer->lines[buffer->size - 1] != NULL) {
-      buffer->start++;
+    if(cb->end == cb->start && cb->lines[cb->size - 1] != NULL) {
+      cb->start++;
     }
 
-    buffer->end++;
+    cb->end++;
   }
 }
 
-char* cbuf_get(cb* buffer) {
-  if(buffer->lines[buffer->start] == NULL) {
+// Get line from cycle buffer
+char* cbuf_get(cycle_buffer_t* cb) {
+  if(cb->lines[cb->start] == NULL) {
     return NULL;
   }
 
-  if(buffer->start + 1 == buffer->size) {
-    buffer->start = 0;
-    return buffer->lines[buffer->size - 1];
+  if(cb->start + 1 == cb->size) {
+    cb->start = 0;
+    return cb->lines[cb->size - 1];
   } else {
-    buffer->start++;
-    return buffer->lines[buffer->start - 1];
+    cb->start++;
+    return cb->lines[cb->start - 1];
   }
 }
 
@@ -105,6 +113,7 @@ int main(int argc, char* argv[]) {
   FILE *file;
   int bufferSize = DEFAULT_SIZE_BUFFER;
 
+  // Parse arguments
   if (argc > 2 && strcmp(argv[1], "-n") == 0) {
     char *endptr;
     bufferSize = strtol(argv[2], &endptr, 10);
@@ -146,13 +155,14 @@ int main(int argc, char* argv[]) {
     exit(EXIT_SUCCESS);
   }
 
-  cb* buffer = cbuf_create(bufferSize);
+  cycle_buffer_t* cb = cbuf_create(bufferSize);
 
   char line[MAX_LINE_LENGTH];
   bool warningPrinted = false;
 
+  // Read lines from file
   while(fgets(line, sizeof(line), file) != NULL) {
-    cbuf_put(buffer, line);
+    cbuf_put(cb, line);
 
     size_t len = strlen(line);
 
@@ -171,15 +181,16 @@ int main(int argc, char* argv[]) {
 
   fclose(file);
 
+  // Print lines from cycle buffer
   do {
-    char* line = cbuf_get(buffer);
+    char* line = cbuf_get(cb);
     if(line == NULL) {
       break;
     }
 
     printf("%s", line);
-  } while (buffer->start != buffer->end && line != NULL);
+  } while (cb->start != cb->end && line != NULL);
 
-  cbuf_free(buffer);
-  exit(EXIT_SUCCESS);
+  cbuf_free(cb);
+  return 0;
 }
